c39ae848e5e092209763489e7e93c5584a987fb5
Rodrigo Fernandes Sacht <conectarodrigo@gmail.com>
2017-07-31 16:40:58 -0300

    Criação da View responsável por informar ao usuário que o registro foi realizado com sucesso e que um e-mail foi disparado para verificação

---
+++ resources/views/email/verification.blade.php
@@ -0,0 +1,14 @@
+@extends ('layouts.app') @section ('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading"> Registro </div>
+                <div class="panel-body">
+                    Você se registrou com sucesso. Um e-mail é enviado para verificação.
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endimento
