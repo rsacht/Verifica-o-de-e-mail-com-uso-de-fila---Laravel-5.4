6f385a12efa3251c7b6c16e6affd995d858c030a
Rodrigo Fernandes Sacht <conectarodrigo@gmail.com>
2017-07-31 15:34:14 -0300

    Criação do Template do e-mail

--- resources/views/email/email.blade.php
+++ resources/views/email/email.blade.php
@@ -1,4 +1,2 @@
 <h1>Confirmação de Cadastro</h1>
-
-Clique no seguinte link para verificar seu email
-{{url(‘/verifyemail/’.$email_token)}}
+Clique no link para confirmar seu cadastro{{url(‘/verifyemail/’.$email_token)}}
