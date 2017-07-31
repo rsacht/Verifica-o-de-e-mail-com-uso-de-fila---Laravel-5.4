9a893bef51e5e939109f09a71a4c5c65b774b220
Rodrigo Fernandes Sacht <conectarodrigo@gmail.com>
2017-07-31 11:38:46 -0300

    Esta classe vem com dois métodos. O primeiro é o construtor () e o segundo é a compilação () que fará a maior parte do trabalho. Ele vincula a vista com o e-mail. Vou enviar um token de usuário junto com a visualização para que o usuário possa ser verificado. Para isso, adicionamos uma nova variável protegida: protected ; e adicionamos a variável  como parametro no construtor.

--- app/Mail/EmailVerification.php
+++ app/Mail/EmailVerification.php
@@ -18,9 +18,9 @@ class EmailVerification extends Mailable
      */
     protected $user;
     
-    public function __construct()
+    public function __construct($user)
     {
-        //
+        $this->user=$user;
     }
 
     /**
